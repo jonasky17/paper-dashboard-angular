b00878278ae6394bc3f68163f11b8dacd9919a36
Jonas Escobar <jonasky17@gmail.com>
2023-11-24 16:57:59 +0800

    test

---
+++ src/app/pages/profiles/profiles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'dashboard-cmp',
+    moduleId: module.id,
+    templateUrl: 'profiles.component.html'
+})
+
+export class ProfilesComponent implements OnInit{
+
+
+    ngOnInit(){
+      alert();
+    }
+}
